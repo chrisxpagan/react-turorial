9cbc4901b1950124052ee62f2f47d79b72cab332
Christopher Pagan <christopherxpagan@gmail.com>
2021-03-14 14:02:06 -0400

    end of 60

--- src/App.js
+++ src/App.js
@@ -21,13 +21,21 @@ class App extends Component {
     this.setState({ persons: persons });
   };
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex((p) => {
+      return p.id === id;
+    });
+
+    // const person = Object.assign({}, this.state.persons[personIndex]);
+    // or:
+    const person = { ...this.state.persons[personIndex] };
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: "Max", age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: "Stephanie", age: 26 },
-      ],
+      persons: persons,
     });
   };
 
@@ -57,6 +65,7 @@ class App extends Component {
                 age={person.age}
                 click={() => this.deletePersonHandler(index)}
                 key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}
               />
             );
           })}
