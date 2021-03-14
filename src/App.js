63693f1a3d2767bb2abe2be3e3dbe349bf7891b7
Christopher Pagan <christopherxpagan@gmail.com>
2021-03-14 15:10:07 -0400

    begin assignment

--- src/App.js
+++ src/App.js
@@ -3,86 +3,8 @@ import Person from "./Person/Person";
 import "./App.css";
 
 class App extends Component {
-  state = {
-    persons: [
-      { id: "sgas", name: "Chris", age: 30 },
-      { id: "gashs", name: "Fiyin", age: 28 },
-      { id: "aeifn", name: "Ghan", age: 27 },
-    ],
-    otherState: "some other value",
-    showPersons: false,
-  };
-
-  deletePersonHandler = (personsIndex) => {
-    // const persons = this.state.persons.slice();
-    // or:
-    const persons = [...this.state.persons];
-    persons.splice(personsIndex, 1);
-    this.setState({ persons: persons });
-  };
-
-  nameChangedHandler = (event, id) => {
-    const personIndex = this.state.persons.findIndex((p) => {
-      return p.id === id;
-    });
-
-    // const person = Object.assign({}, this.state.persons[personIndex]);
-    // or:
-    const person = { ...this.state.persons[personIndex] };
-    person.name = event.target.value;
-
-    const persons = [...this.state.persons];
-    persons[personIndex] = person;
-
-    this.setState({
-      persons: persons,
-    });
-  };
-
-  togglePersonsHandler = () => {
-    const doesShow = this.state.showPersons;
-    this.setState({ showPersons: !doesShow });
-  };
-
   render() {
-    const style = {
-      backgroundColor: "white",
-      font: "inherit",
-      border: "1px solid blue",
-      padding: "8px",
-      cursor: "pointer",
-    };
-
-    let persons = null;
-
-    if (this.state.showPersons) {
-      persons = (
-        <div>
-          {this.state.persons.map((person, index) => {
-            return (
-              <Person
-                name={person.name}
-                age={person.age}
-                click={() => this.deletePersonHandler(index)}
-                key={person.id}
-                changed={(event) => this.nameChangedHandler(event, person.id)}
-              />
-            );
-          })}
-        </div>
-      );
-    }
-
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <p>this is really working!</p>
-        <button onClick={this.togglePersonsHandler} style={style}>
-          Toggle Persons
-        </button>
-        {persons}
-      </div>
-    );
+    return <div className="App"></div>;
   }
 }
 
